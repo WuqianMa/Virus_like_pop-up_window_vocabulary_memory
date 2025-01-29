dd78afc3e58be2702a426b69aa82f0862e0e5c61
wuqian <wuqian.ma@efrei.net>
2025-01-29 09:44:59 +0100

    Deploy to Github Page

---
+++ docs/vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+    plugins: [react()],
+    base: "./", // Ensure relative paths for assets
+    build: {
+        outDir: "docs", // Output files to /docs instead of /dist
+    },
+});
